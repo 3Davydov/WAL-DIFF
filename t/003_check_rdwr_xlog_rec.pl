1fb2e93e4cf2ca1a31b6679ea6beb2060bf00341
3Davydov <davydovdaniil501@gmail.com>
2024-05-23 22:05:57 +0700

    add comments to .pl test file

--- t/003_check_rdwr_xlog_rec.pl
+++ t/003_check_rdwr_xlog_rec.pl
@@ -25,9 +25,11 @@ $node->append_conf(
 # Start the PostgreSQL server
 $node->start;
 
-# setup
+# Setup
 $node->safe_psql('postgres', 'CREATE EXTENSION wal_diff');
 
+# Create table wich contain writer_state struct, because we
+# need to pass it through processes
 $node->safe_psql(
         'postgres', 
         "CREATE TABLE writer_state (
@@ -57,11 +59,14 @@ $node->safe_psql(
         last_written_rec_len bigint
         );"
 );
+
+# Insert initial values
 $node->safe_psql(
     'postgres', 
     "INSERT INTO writer_state (id, src_fd_pos, dest_curr_offset, dest_dir) VALUES (1, 0, 0, '');"
 );
 
+# Create wal/wal diff file names
 my $wal_file = $node->data_dir . '/pg_wal/000000010000000000000001';
 my $wal_diff_file = $node->data_dir . '/wal_diff/000000010000000000000001';
 ok(-f -e $wal_file, "Got a wal file");
@@ -70,6 +75,8 @@ my $read_record;
 my $total_read_count = 0;
 my $num_records = 10;
 
+# Read record from wal segment and write it to wal_diff
+# segment $num_records times
 for (my $iter = 0; $iter < $num_records; $iter++) {
     eval {
         $read_record = $node->safe_psql(
@@ -107,85 +114,10 @@ for (my $iter = 0; $iter < $num_records; $iter++) {
     };
 }
 
-# eval {
-#     $read_record = $node->safe_psql(
-#         'postgres', 
-#         "SELECT read_xlog_rec('$wal_file');"
-#     );
-#     1;
-# } or do {
-#     my $error = $@ || 'Unknown error';
-#     $node->stop('immediate');
-#     BAIL_OUT("Failed to execute 'read_xlog_rec' ");
-# };
-
-# my $binary_string = pack('H*', substr($read_record, 2));
-# my @rec = unpack( 'H*', $binary_string );
-# @rec = map { $_ } @rec;
-
-# my @new_array;
-# for (my $i = 0; $i < length(@rec[0]); $i+=2) {
-#     push @new_array, substr(@rec[0], $i, 2);
-# }
-
-# $total_read_count += scalar @new_array;
-
-# eval {
-#     $node->safe_psql(
-#         'postgres', 
-#         "SELECT write_xlog_rec('$wal_diff_file', '$read_record');"
-#     );
-#     1;
-# } or do {
-#     my $error = $@ || 'Unknown error';
-#     $node->stop('immediate');
-#     BAIL_OUT("Failed to execute 'write_xlog_rec' ");
-# };
-
-# sleep(1);
-
-####
-# eval {
-#     $read_record = $node->safe_psql(
-#         'postgres', 
-#         "SELECT read_xlog_rec('$wal_file');"
-#     );
-#     1;
-# } or do {
-#     my $error = $@ || 'Unknown error';
-#     $node->stop('immediate');
-#     BAIL_OUT("Failed to execute 'read_xlog_rec' ");
-# };
-
-# $binary_string = pack('H*', substr($read_record, 2));
-# @rec = unpack( 'H*', $binary_string );
-# @rec = map { $_ } @rec;
-
-# @new_array = ();
-# for (my $i = 0; $i < length(@rec[0]); $i+=2) {
-#     push @new_array, substr(@rec[0], $i, 2);
-# }
-
-# $total_read_count += scalar @new_array;
-
-# eval {
-#     $node->safe_psql(
-#         'postgres', 
-#         "SELECT write_xlog_rec('$wal_diff_file', '$read_record');"
-#     );
-#     1;
-# } or do {
-#     my $error = $@ || 'Unknown error';
-#     $node->stop('immediate');
-#     BAIL_OUT("Failed to execute 'write_xlog_rec' ");
-# };
-
-# sleep(1);
-####
-
 my $size = -s $wal_diff_file;
 ok($size == $total_read_count, "Read bytes == write bytes");
 
+# Read all read wal and all written wal_diff
 my ($buffer1, $buffer2);
 open(my $fh1, '<:raw', $wal_file) or die "Cannot open file '$wal_file': $!";
 open(my $fh2, '<:raw', $wal_diff_file) or die "Cannot open file '$wal_diff_file': $!";
@@ -194,6 +126,7 @@ my $bytes_read2 = read($fh2, $buffer2, $total_read_count);
 close($fh1);
 close($fh2);
 
+# Try to find positions, where wal differ wal_diff
 my $is_equal = 1;
 for (my $i = 0; $i < $total_read_count; $i++) {
     if (substr($buffer1, $i, 1) ne substr($buffer2, $i, 1)) {
